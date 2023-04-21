b5cc05c9e83501cd1037e28bffaf87da9c179307
Ra12-kumar <88586633+Ra12-kumar@users.noreply.github.com>
2023-04-21 08:38:18 +0530

    Add files via upload

---
+++ Main (6).cpp
@@ -0,0 +1,14 @@
+// Pattern problem
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n=5;
+  for(int row=1;row<=5;row++){
+    for(int col=1;col<=5;col++){
+      cout<<"1 ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
